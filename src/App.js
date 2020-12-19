90761a4cbe4a53490bb8ea3c66f6bef5bba46d32
Yurii Konoval <yuriioleksandrovichkonoval@gmail.com>
2020-12-20 00:14:31 +0200

    hw2

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { Component } from 'react';
+import './App.css';
+import shortid from 'shortid';
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const id = shortid.generate();
+    const contact = {
+      id: id,
+      name: name,
+      number: number,
+    };
+    const { contacts } = this.state;
+
+    contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
+      ? alert(`${name} is already in contacts`)
+      : this.setState(() => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => id !== contact.id),
+    }));
+  };
+
+  filterChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const normilizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normilizedFilter),
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <div className="container">
+        <h1 className="title">Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <p className="title statistics">Statistics</p>
+        <Filter value={filter} onChange={this.filterChange} />
+        <ContactList contacts={visibleContacts} onDelete={this.deleteContact} />
+      </div>
+    );
+  }
+}
+export default App;
